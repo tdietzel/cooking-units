71090687fc83cff0e5d96e05023f06fc866e2088
tdietzel <dietzelbiz@outlook.com>
2023-03-21 10:54:58 -0700

    add function to converts gallons to liters

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,19 @@
-function ml(tsp,tbsp,cups) {
-  return tsp*4.929 + tbsp*14.787 + cups*236.588;
+function liters(gallon) {
+  return gallon*3.785;
 }
 
-const tsp = prompt("Enter amount of Teaspoons: ");
-const tbsp = prompt("Enter amount of Tablespoons: ");
-const cups = prompt("Enter amount of Cups: ");
+const gallon = prompt("Enter amount of gallons to convert to liters: ");
 
-window.alert("The total amount of TSP, TBSP and CUPS in ML: " + ml(tsp,tbsp,cups) + ".");
+window.alert("The total amound of liters: " + liters(gallon) + ".");
+//function ml(tsp,tbsp,cups) {
+  //return tsp*4.929 + tbsp*14.787 + cups*236.588;
+//}
 
+//const tsp = prompt("Enter amount of Teaspoons: ");
+//const tbsp = prompt("Enter amount of Tablespoons: ");
+//const cups = prompt("Enter amount of Cups: ");
 
-
-
-
-
-
-
-
-
+//window.alert("The total amount of TSP, TBSP and CUPS in ML: " + ml(tsp,tbsp,cups) + ".");
 
 
 //function grams(ounces,pounds) {
