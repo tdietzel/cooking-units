b6d7d232a6e84cb325a9883b0b7da353355a6619
tdietzel <dietzelbiz@outlook.com>
2023-03-21 10:44:18 -0700

    add function to voncert between tsp, tbps & cups to ml

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,29 @@
-function grams(ounces,pounds) {
-  return ounces*28.35 + pounds*453.592;
+function ml(tsp,tbsp,cups) {
+  return tsp*4.929 + tbsp*14.787 + cups*236.588;
 }
 
-const pounds = prompt("Enter Pounds: ");
-const ounces = prompt("Enter Ounces: ");
+const tsp = prompt("Enter amount of Teaspoons: ");
+const tbsp = prompt("Enter amount of Tablespoons: ");
+const cups = prompt("Enter amount of Cups: ");
 
-window.alert("The total of Ounces & Pounds in Grams is: " + grams(ounces,pounds) + ".");
+window.alert("The total amount of TSP, TBSP and CUPS in ML: " + ml(tsp,tbsp,cups) + ".");
+
+
+
+
+
+
+
+
+
+
+
+
+//function grams(ounces,pounds) {
+  //return ounces*28.35 + pounds*453.592;
+//}
+
+//const pounds = prompt("Enter Pounds: ");
+//const ounces = prompt("Enter Ounces: ");
+
+//window.alert("The total of Ounces & Pounds in Grams is: " + grams(ounces,pounds) + ".");
